073fc5a6febb6b7cc48350e4ebb4efe82d23cb0f
Jesus <r.jesus9107@gmail.com>
2022-02-21 20:38:10 -0800

    removed frontend serve

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { connectDB } from './config/db.js';
-import path from 'path';
+
 import accounts from './routes/accounts.route.js';
 import users from './routes/users.route.js';
 import categories from './routes/categories.route.js';
@@ -12,7 +12,6 @@ import transactions from './routes/transactions.route.js';
 dotenv.config();
 
 const port = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 connectDB();
 
@@ -26,18 +25,6 @@ app.use("/api/v1/accounts", accounts);
 app.use("/api/v1/users", users);
 app.use("/api/v1/categories", categories);
 app.use("/api/v1/transactions", transactions);
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "frontend", "build", "index.html")
-        );
-    });
-} else {
-    app.get("/", (req, res) => res.send("Set to production"));
-}
 
 app.use("*", (req, res) => res.status(400).json({ error: "not found"}));
 app.use(errorHandler);
